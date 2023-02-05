f0f0fabd6797faa2928b412c066f59bef4bea270
Magnus <mp-se@users.noreply.github.com>
2023-02-05 18:53:21 +0100

    Update unit test

--- test/tests_level.cpp
+++ test/tests_level.cpp
@@ -24,8 +24,11 @@ SOFTWARE.
 #include <AUnit.h>
 #include <levelraw.hpp>
 #include <log.hpp>
+#include <main.hpp>
+#include <kegconfig.hpp>
 
-RawLevelDetection raw;
+RawLevelDetection raw(UnitIndex::U1, 1, 1, 1);
+KegConfig myConfig("TEST", "TEST");
 
 test(level_raw) {
   float data[10] = { 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9 };
@@ -35,29 +38,29 @@ test(level_raw) {
   assertEqual(raw.hasRawValue(), false);
   assertEqual(raw.hasAverageValue(), false);
 
-  raw.add( data[0] );
+  raw.add( data[0], 0 );
   assertEqual(raw.getRawValue(), data[0]);
   assertEqual(raw.average(), data[0]);
   assertEqual(raw.sum(), data[0]);
 
-  raw.add( data[1] );
+  raw.add( data[1], 0 );
   sum = data[0] + data[1];
   assertEqual(raw.getRawValue(), data[1]);
   assertEqual(raw.average(), sum/2);
   assertEqual(raw.sum(), sum);
 
-  raw.add( data[2] );
-  raw.add( data[3] );
-  raw.add( data[4] );
-  raw.add( data[5] );
-  raw.add( data[6] );
-  raw.add( data[7] );
+  raw.add( data[2], 0 );
+  raw.add( data[3], 0 );
+  raw.add( data[4], 0 );
+  raw.add( data[5], 0 );
+  raw.add( data[6], 0 );
+  raw.add( data[7], 0 );
   sum = data[0] + data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + data[7];
   assertEqual(raw.getRawValue(), data[7]);
   assertEqual(raw.sum(), sum);
 
   // Store last 8 values, so data[0] should be dropped.
-  raw.add( data[8] );
+  raw.add( data[8], 0 );
   sum = data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + data[7] + data[8];
   assertEqual(raw.getRawValue(), data[8]);
   assertEqual(raw.sum(), sum);
