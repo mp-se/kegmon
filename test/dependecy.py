15df0f04ab5bd24d7d625d23737802aaed5faf03
Magnus <mp-se@users.noreply.github.com>
2023-09-10 22:32:02 +0200

    Updated dependency checker

--- test/dependecy.py
+++ test/dependecy.py
@@ -1,7 +1,11 @@
 import requests
+import configparser
+import json
+
+debug = False
+useFiles = False
 
 githubUrl = "https://api.github.com/repos/"
-debug = False
 
 config = { 
    "repos": [
@@ -13,7 +17,6 @@ config = {
       "mp-se/Adafruit_Sensor",
       "mp-se/incbin",
       "mp-se/Arduino-Log",
-      "mp-se/ArduinoJson",
       "mp-se/arduino-mqtt",
       "mp-se/tinyexpr",
       "mp-se/NAU7802_Arduino_Library",
@@ -25,7 +28,7 @@ config = {
     ],
 }
 
-def get_latest_tag(repo):
+def get_github_latest_tag(repo):
     if repo == "":
         return ""
 
@@ -47,7 +50,7 @@ def get_latest_tag(repo):
     # print("Error:", "No releases found for:", repo)
     return ""
 
-def get_parent(repo):
+def get_github_parent(repo):
     if repo == "":
         return ""
 
@@ -64,15 +67,89 @@ def get_parent(repo):
 
     return ""
       
-
-if __name__ == '__main__':
+def get_github_repo_version():
+  deps = []
 
   for repo in config["repos"]:    
-    tag = get_latest_tag(repo)
-    parent = get_parent(repo)
-    ptag = get_latest_tag(parent)
+    tag = get_github_latest_tag(repo)
+    parent = get_github_parent(repo)
+    ptag = get_github_latest_tag(parent)
+    dep = { "repo": repo, "tag": tag, "parent": parent, "parentTag": ptag }
+    deps.append(dep)
 
-    status = ""
-    if tag != ptag: status = "CHECK!"
-      
-    print("Dependency:", repo, tag, parent, ptag, status)
+  return deps
+
+def resolve_platformio_key(config, key):
+  key = key.removeprefix("${")
+  key = key.removesuffix("}")
+  libs = config.get(key.split(".")[0], key.split(".")[1])
+  return libs.split()
+
+def find_platformio_libs():
+  config = configparser.ConfigParser()
+  config.read('platformio.ini')
+
+  depList = []
+
+  for section in config.sections():
+    if section.startswith("env:"):
+      libs = config.get(section, "lib_deps" )
+      for lib in libs.split():
+        if lib.startswith("${") and lib.endswith("}"):
+          depList = depList + resolve_platformio_key(config, lib)
+        else:
+          depList.append(lib)
+
+  # Remove duplicated entries
+  depList = list(set(depList))
+
+  dependecies = []
+
+  for dep in depList:
+    s = dep.removeprefix("https://github.com/").split("#")
+    if len(s) == 2:
+      e = { "repo": s[0], "tag": s[1] }
+      dependecies.append(e)
+    else:
+      e = { "repo": s[0], "tag": "" }
+      dependecies.append(e)
+
+  # Entires have format; repository # tag 
+  return dependecies
+
+def write_json(file, data):
+  f = open(file, "w")
+  f.write( json.dumps(data, indent=2) )
+  f.close()
+
+def read_json(file):
+  f = open(file, "r")
+  data = json.loads( "".join(f.readlines()) )
+  f.close()
+  return data
+
+
+if __name__ == '__main__':
+  print("Dependency checker")
+
+  print("Reading platformio configuration")
+  if useFiles: 
+    depLibs = read_json("project_deps.json")
+  else:
+    depLibs = find_platformio_libs()
+    write_json("project_deps.json", depLibs)
+
+  print("Fetching data from github on latest tags")
+  if useFiles: 
+    repos = read_json("repos.json")
+  else:
+    repos = get_github_repo_version()
+    write_json("repos.json", repos)
+
+  for i in depLibs:
+    for j in repos:
+      if i["repo"] == j["repo"]:
+        if i["tag"] != j["tag"]:
+          print("Check dependency for: ", i["repo"], i["tag"], " => ", j["tag"] )
+
+  print("Done...")
